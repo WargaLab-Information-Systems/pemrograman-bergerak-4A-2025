616c70a5375fcd766504975a1f69c25fa93edfd8
naufalfahmiii <naufal.fahmi0423@gmail.com>
2025-03-20 23:34:15 +0700

    tugas_exam_dart_23_042_MohammadNaufalFahmi_Asprak_MasTamam

--- tugas-dart/dart-submission_23_042_MohammadNaufalFahmi/bin/exam3/exam3.dart
+++ tugas-dart/dart-submission_23_042_MohammadNaufalFahmi/bin/exam3/exam3.dart
@@ -1,16 +1,20 @@
 Set uniqueElement(List<int> myList) {
   // TODO 1
 
-  return {};
-
+  return myList.toSet();
   // End of TODO 1
 }
 
 Map<String, String> buildFutsalPlayersMap() {
   // TODO 2
 
-  return {};
-
+  return {
+    "Goalkeeper": "Andri", 
+    "Anchor": "Irfan", 
+    "Pivot": "Fikri", 
+    "Right Flank": "Aldi", 
+    "Left Flank": "Hafid"
+  };
   // End of TODO 2
 }
 
@@ -18,8 +22,7 @@ Map<String, String> updatePivotPlayer() {
   final futsalPlayers = buildFutsalPlayersMap();
 
   // TODO 3
-
-  // End of TODO 3
-
+  futsalPlayers["Pivot"] = "Fajar";
   return futsalPlayers;
+  // End of TODO 3
 }
